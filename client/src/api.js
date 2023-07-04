30b7fa748a52d13bf17a3c32a3345c84a1c11cfd
ArcherLW <archerwann@gmail.com>
2023-07-03 19:44:29 -0600

    Added post, put, and delete support

--- client/src/api.js
+++ client/src/api.js
@@ -7,3 +7,35 @@ function getById(id) {
     return fetch(`/api/employees/${id}`)
         .then((response) => response.json());
 }
+
+function postNewEmployee(employee) {
+    const formData = new FormData();
+    formData.append('name', employee.name);
+    formData.append('title', employee.title);
+    formData.append('avatar', employee.avatar);
+
+    return fetch('/api/employees', {
+        method: 'POST',
+        body: formData
+    }).then((response) => response.json());
+}
+
+function updateById(id, employee) {
+    const formData = new FormData();
+    formData.append('name', employee.name);
+    formData.append('title', employee.title);
+    formData.append('avatar', employee.avatar);
+
+    return fetch(`/api/employees/${id}`, {
+        method: 'PUT',
+        body: formData
+    }).then((response) => response.json());
+}
+
+function deleteById(id) {
+    return fetch(`/api/employees/${id}`, {
+        method: 'DELETE'
+    }).then((response) => response.json());
+}
+
+export {getAll, getById, postNewEmployee, updateById, deleteById};
