d31162ef441340ae9164fa1fa9892c2948221d18
ArcherLW <archerwann@gmail.com>
2023-07-03 18:03:24 -0600

    Simplified App framework

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import EmployeeList from './employeelist'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is the employee list: count:0
-          <EmployeeList></EmployeeList>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Earn React
-        </a>
-      </header>
+      <div>
+        This is the employee list: count:0
+        <EmployeeList></EmployeeList>
+      </div>
     </div>
   );
 }
