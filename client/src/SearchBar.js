ff189edba0c363805f569e3532b8386796aded86
ArcherLW <archerwann@gmail.com>
2023-08-02 14:33:12 -0600

    Added active/inactive toggle

--- client/src/SearchBar.js
+++ client/src/SearchBar.js
@@ -3,14 +3,23 @@ import './SearchBar.css';
 
 function SearchBar(props) {
     const [searchTerms, setSearchTerms] = useState("");
+    const [inactiveOnly, setInactiveOnly] = useState("");
 
     async function handleSubmit(event) {
         event.preventDefault();
-        props.onSearch(searchTerms);
+        props.onSearch(searchTerms, inactiveOnly);
     }
 
     return (
         <form id="searchbar" onSubmit={handleSubmit}>
+            <label id="searchbar-label-active" htmlFor="searchbar-input-active">
+                Inactive Only:
+            </label>
+            <input
+                type="checkbox"
+                id="searchbar-input-active"
+                value={inactiveOnly}
+                onChange={(e) => setInactiveOnly(e.target.value)} />
             <input
                 class="text-entry"
                 type="text"
